5584db2097a14d93c62ee71336d4c2fd323fe3cf
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-11-13 00:13:46 +0200

    Решена задача №03

--- task-07-03.js
+++ task-07-03.js
@@ -16,49 +16,28 @@ const images = [
     },
 ];
 
-/* const creatImagesList = images =>{
-    const containerImagesRef = document.createElement(`ul`); //создает элемент ul
-    const linkImagesRef = document.createElement(`li`); //создает элемент списка li
-    linkImagesRef.textContent = images; //добавляет в список текст-содержимое
-    containerImagesRef.append(linkImagesRef); //добавляет li в ul
-    return containerImagesRef;
-}
-    
-const creatImagesListRootRef = document.querySelector('#gallery');//добавляет созданный элемент в DOM
-    
-creatImagesListRootRef.insertAdjacentHTML('afterbegin', <li>containerImagesRef</li>) */
-/* const containerImagesRef = document.createElement(`ul`); //создает элемент ul
-console.log(containerImagesRef);
-const linkImagesRef = document.createElement(`li`); //создает элемент списка li
-console.log(linkImagesRef);
-
-const image1 = document.createElement('image');
-image1.setAttribute('src', 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-image1.setAttribute('alt', 'White and Black Long Fur Cat');
-console.log(image1);
-
-const image2 = document.createElement('image');
-image2.setAttribute('src', 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-image2.setAttribute('alt', 'Orange and White Koi Fish Near Yellow Koi Fish');
-console.log(image2);
-
-const image3 = document.createElement('image');
-image3.setAttribute('src', 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-image3.setAttribute('alt', 'Group of Horses Running');
-console.log(image3);
-
-const creatImagesListRootRef = document.querySelector('#gallery');//добавляет созданный элемент в DOM
-creatImagesListRootRef.insertAdjacentHTML('afterbegin', '<li>image1</li>'); */
-
 const creatImagesListRootRef = document.querySelector('#gallery');
 images.forEach(element => {
     console.log(element);
     const linkListImagesRef = document.createElement(`li`); //создает элемент списка li
     const linkImagesRef = document.createElement(`img`); //создает элемент списка img
-    linkImagesRef.insertAdjacentHTML('afterbegin', element);
-    creatImagesListRootRef.append(linkListImagesRef);
+    linkImagesRef.insertAdjacentHTML('afterbegin', element);//добавляет элемент в начало списка
+    creatImagesListRootRef.append(linkListImagesRef);//вставляет узел после ('#gallery')
     linkListImagesRef.append(linkImagesRef);
-    linkImagesRef.setAttribute('src', element.url);
-    linkImagesRef.setAttribute('alt', element.src);
+    linkImagesRef.setAttribute('src', element.url);//добавляет атрибут
+    linkImagesRef.setAttribute('alt', element.src);//добавляет атрибут
+    linkListImagesRef.classList.add('image-name');
 }    
 )
+
+//Задание
+/* Напиши скрипт для создания галлереи изображений по массиву данных.
+
+В HTML есть список ul#gallery.
+
+<ul id="gallery"></ul>
+Используй массив объектов images для создания тегов img вложенных в li. 
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы. */
